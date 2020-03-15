8d885ce5833ed0d75d1fb0437e2283076a0d6d95
Aleksi <a12345@live.fi>
2020-03-15 18:35:38 +0200

    Created TopBar using Bootstrap-navbar

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <TopBar />
   </div>
 </template>
 
 <script>
 /* import HelloWorld from './components/HelloWorld.vue' */
+import TopBar from './components/TopBar'
 
 export default {
   name: 'App',
   components: {
     /* HelloWorld */
+    TopBar
   }
 }
 </script>
